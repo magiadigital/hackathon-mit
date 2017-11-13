dabac0f4a7e6a36fb976108a154acd07926c043c
Alfredo Escalante <fr3d0@Desarrollos-MacBook-Pro.local>
2017-11-13 16:25:05 -0500

    results screen donde

--- web/src/app/result/result.component.ts
+++ web/src/app/result/result.component.ts
@@ -24,16 +24,60 @@ export class ResultComponent implements OnInit {
   }
   setResults() {
     this.sharing.setLoader(true);
-    this.data.getCandidatesLedgers().subscribe(ledgers => {
-      this.best.push(ledgers[0]);
-      this.best.push(ledgers[1]);
-      this.best.push(ledgers[2]);
+    this.data.getCandidatos().toPromise()
+      .then(candidates => {
+        for (let i = 0; i < candidates.length; i++) {
+          const candidato = {
+            datos: null,
+            partido: null,
+            $class: candidates[i].$class,
+            dni: candidates[i].dni
+          };
+          this.data.getCiudadanoByCandidato(candidates[i]).toPromise().then(citizens => {
+            if (citizens.length > 0) {
+              candidato.datos = citizens[0];
+              this.data.getPartidoByCandidato(candidates[i]).toPromise().then( partidos => {
+                if (partidos.length > 0) {
+                  candidato.partido = partidos[0];
+                }
+                this.candidates.push(candidato);
+                if ((i + 1) === candidates.length) {
+                  this.data.getCandidatesLedgers().subscribe(ledgers => {
+                    ledgers.forEach(ledger => {
+                      this.candidates.forEach(candidate => {
+                        if (ledger.candidato.split('#')[1] === candidate.dni) {
+                          candidate.ledger = ledger;
+                        }
+                      });
+                    });
+                    this.candidates = this.sortArray(this.candidates);
+                    this.best.push(this.candidates[0]);
+                    this.best.push(this.candidates[1]);
+                    this.best.push(this.candidates[2]);
 
-      for (let i = 2; i < ledgers.length; i++) {
-        this.others.push(ledgers[i]);
+                    for (let j = 3; j < this.candidates.length; j++) {
+                      this.others.push(this.candidates[j]);
+                    }
+                    this.sharing.setLoader(false);
+                  });
+                }
+              });
+            }
+          });
+        }
+      });
+  }
+  sortArray(array) {
+    array.sort((a: any, b: any) => {
+      if (a.ledger.total > b.ledger.total) {
+      return -1;
+      } else if (a.ledger.total < b.ledger.total) {
+      return 1;
+      } else {
+      return 0;
       }
-      this.sharing.setLoader(false);
     });
+    return array;
   }
 
 }
